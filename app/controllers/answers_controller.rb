daf822c96fbc59f608654b359c33a3d29f427c2f
uirobot <d.shuleikin@gmail.com>
2015-04-02 11:24:33 +0300

    Ajax answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -9,12 +9,7 @@ class AnswersController < ApplicationController
   def create
     @answer = @question.answers.create(answer_params)
     @answer.user = current_user
-    if @answer.save
-      flash[:notice] = 'Your answer added'
-      redirect_to @question
-    else
-      render :new
-    end
+    @answer.save
   end
 
   def destroy
