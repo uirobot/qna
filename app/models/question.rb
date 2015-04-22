670f38e3422da516527409a9e6660b7223f01b71
uirobot <d.shuleikin@gmail.com>
2015-04-22 12:22:57 +0300

    add gem and model test and implemetation for attachable

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,5 @@ class Question < ActiveRecord::Base
   has_many :answers, dependent: :destroy
   belongs_to :user
   validates :title, :body, presence: true
+  has_many :attachments, as: :attachable
 end
