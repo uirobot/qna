c2771df1d79116af4210b601cda6fbf8211dfee6
uirobot <d.shuleikin@gmail.com>
2015-04-26 13:18:03 +0300

    Can add answer multiple

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -41,7 +41,7 @@ class AnswersController < ApplicationController
   private
 
   def answer_params
-    params.require(:answer).permit(:body, attachments_attributes: [:file])
+    params.require(:answer).permit(:body, attachments_attributes: [:id, :file, :_destroy])
   end
 
   def load_question
