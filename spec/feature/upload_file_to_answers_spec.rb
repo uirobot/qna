87d8756f6a1486889f8bfebeedc1b7548033550d
uirobot <d.shuleikin@gmail.com>
2015-04-26 14:23:54 +0300

    Multiple files add to question and answers tests

--- spec/feature/upload_file_to_answers_spec.rb
+++ spec/feature/upload_file_to_answers_spec.rb
@@ -22,4 +22,19 @@ feature 'Add files to answer', %q{
       expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/1/rails_helper.rb'
     end
   end
+
+  scenario 'User adds multiple file when post answer', js: true do
+    fill_in 'Body', with: 'its a answer body'
+    attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+    click_on 'add file'
+    within all('.add_file_form').last do
+      attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    end
+    click_on 'Send answer'
+    within '.answers-list' do
+      expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/1/rails_helper.rb'
+      expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/2/spec_helper.rb'
+    end
+  end
+
 end
--- spec/feature/upload_file_to_question_spec.rb
+++ spec/feature/upload_file_to_question_spec.rb
@@ -20,4 +20,18 @@ feature 'Add files to question', %q{
     click_on 'Send question'
     expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
   end
+
+  scenario 'User can add multiple files to question', js: true do
+    fill_in 'Title', with: 'its a title'
+    fill_in 'Body', with: 'this is question body'
+    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    click_on 'add file'
+
+    within all('.add_file_form').last do
+      attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+    end
+    click_on 'Send question'
+    expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+    expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/2/rails_helper.rb'
+  end
 end
