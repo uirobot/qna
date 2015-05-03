0d596fae33dcbcdad43c72ba6dba869929468118
uirobot <d.shuleikin@gmail.com>
2015-05-03 10:52:11 +0300

    Ajax question edition

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -33,11 +33,7 @@ class QuestionsController < ApplicationController
   end
 
   def update
-    if @question.update(question_params)
-      redirect_to @question
-    else
-      render :edit
-    end
+    render :edit unless @question.update(question_params)
   end
 
   def destroy
